da97d8bb2980a3d44446a6e02da5bcae7e75e391
Yagyik88 <raghuwanshiyagyik@gmail.com>
2025-05-28 21:33:52 +0530

    Update MapComponent.jsx

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,12 +1,11 @@
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-
 const Map = ({ location }) => {
   if (!location) return <div className="map-error">No location data</div>;
 
   return (
     <LoadScript 
-      googleMapsApiKey="AIzaSyCLm6-I9DhHzyQZ2541ONxD6d7thNxqV4A" 
+      googleMapsApiKey={process.env.REACT_APP_API_KEY}
       loadingElement={<div className="map-loading">Loading Map...</div>}
     >
       <GoogleMap
@@ -21,5 +20,3 @@ const Map = ({ location }) => {
 };
 
 export default Map;
-
-
